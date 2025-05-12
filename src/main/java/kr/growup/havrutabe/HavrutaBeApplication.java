f07812a678ec2a4abe546d0ba7a0704121534c2b
thornscript <thornscript.reliable417@passmail.net>
2025-05-12 20:06:15 +0900

    first commit

---
+++ src/main/java/kr/growup/havrutabe/HavrutaBeApplication.java
@@ -0,0 +1,13 @@
+package kr.growup.havrutabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavrutaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HavrutaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/growup/havrutabe/HavrutaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.growup.havrutabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavrutaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
